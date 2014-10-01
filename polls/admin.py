644cf49036818c744edddffa423e6e114767370e
Nick Satterly <nick.satterly@guardian.co.uk>
2014-10-01 14:57:58 +0100

    part 2 - choice page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import  Choice, Poll
 
 # Register your models here.
-admin.site.register(Poll)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
