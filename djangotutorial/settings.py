f45f6b8bc944e01b9ed1da4ce58dd879d593adef
Nick Satterly <nick.satterly@guardian.co.uk>
2014-10-01 14:47:11 +0100

    remove db router

--- djangotutorial/settings.py
+++ djangotutorial/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TIME_ZONE = 'Europe/London'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -56,14 +57,10 @@ WSGI_APPLICATION = 'djangotutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASE_ROUTERS = ['djangotutorial.dbrouter.EnvironmentRouter']
+# DATABASE_ROUTERS = ['djangotutorial.dbrouter.EnvironmentRouter']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'dev': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tutorial',
         'USER': 'django',
