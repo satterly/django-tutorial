ecadbe004e0be817ac57ce4289c8de0dbadb1993
Nick Satterly <nick.satterly@guardian.co.uk>
2014-10-01 15:36:33 +0100

    part 2 - customize admin site

--- djangotutorial/settings.py
+++ djangotutorial/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+print BASE_DIR
+
 TIME_ZONE = 'Europe/London'
 
 # Quick-start development settings - unsuitable for production
@@ -27,6 +29,9 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+print TEMPLATE_DIRS
 
 # Application definition
 
