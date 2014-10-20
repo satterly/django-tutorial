d5d8f97b5a8a0361feb72eef2c17b92bb4523fd9
Nick Satterly <nick.satterly@guardian.co.uk>
2014-10-20 17:40:56 +0100

    part 3 - simplify the URLconfs

--- djangotutorial/urls.py
+++ djangotutorial/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+urlpatterns = patterns('polls.views',
+    url(r'^polls/$', 'index'),
+    url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
+)
 
+urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
